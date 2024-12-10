113d13c393dcd15b1102f0d7e4d1fbb79e3403bb
João Gabriel Pieroli da Silva <joaogabrielpieroli18@gmail.com>
2024-12-10 11:04:21 -0300

    main sem debugers

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     int op;
     int tipo;
 
-    scanf("%d", &tipo); /*0 -> ED1 (AVL), 1 -> ED2 (Rubro-Negra)*/
+    scanf("%d", &tipo); /*0 -> ED1 (AVL), 1 -> ED2 (LLRBT)*/
 
     A = set_criar(tipo);
     B = set_criar(tipo);
@@ -21,14 +21,12 @@ int main(int argc, char *argv[])
     {
         scanf("%d", &x);
         set_inserir(A, x);
-        set_imprimir(A);
     }
     // São inseridos os elementos em B
     for (int i = 0; i < n_b; i++)
     {
         scanf("%d", &x);
         set_inserir(B, x);
-        set_imprimir(B);
     }
 
     // Escolha da operação
@@ -36,15 +34,12 @@ int main(int argc, char *argv[])
     switch (op) {
         case 1: // PERTERCE
         {
-            set_imprimir(A);
             scanf("%d", &x);
             set_pertence(A,x) ? printf("Pertence.\n") : printf("Não pertence.\n");
             break;
         }
         case 2: // UNIÃO
         {
-            set_imprimir(A);
-            set_imprimir(B);
             SET *C = set_uniao(A, B);
             set_imprimir(C);
             set_apagar(&C);
@@ -52,8 +47,6 @@ int main(int argc, char *argv[])
         }
         case 3: // INTERSEÇÃO
         {
-            set_imprimir(A);
-            set_imprimir(B);
             SET *C = set_intersecao(A, B);
             set_imprimir(C);
             set_apagar(&C);
@@ -61,7 +54,6 @@ int main(int argc, char *argv[])
         }
         case 4: // REMOVER
         {
-            set_imprimir(A);
             scanf("%d", &x);
             set_remover(A, x);
             set_imprimir(A);
